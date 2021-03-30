46507179ba36de2165c92a41466846f41b5f8202
Seth Torek <seth.torek@gmail.com>
2021-03-30 18:39:06 -0400

    used git fetch for pet doption api

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+function firstFetch(token) {
+  var queryURL = "https://api.petfinder.com/v2/animals?breed=pug";
+
+  fetch(queryURL, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    }).catch(function (err) {
+        // Log any errors
+        console.log("something went wrong", err);
+      });
+}
+
+
+
+var key = "oxrPyoy6v3XMn43E8m5y5ZVOEGAmTO52CKOvjV7CckXTDJvpjG";
+var secret = "AYuKkVCKqFIYCOxKzBWeihxy7lA7vSOReHMlLC5E";
+
+fetch("https://api.petfinder.com/v2/oauth2/token", {
+  method: "POST",
+  body:
+    "grant_type=client_credentials&client_id=" +
+    key +
+    "&client_secret=" +
+    secret,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+})
+  .then(function (resp) {
+    // Return the response as JSON
+    return resp.json();
+  })
+  .then(function (data) {
+    // Log the API data
+    console.log("token", data);
+    firstFetch(data.access_token);
+  })
+  .catch(function (err) {
+    // Log any errors
+    console.log("something went wrong", err);
+  });
