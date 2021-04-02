4f9f2e275ffb69ada2d10abd0cb1c1083290b09e
Nicholas Ganim <nicog99@gmail.com>
2021-04-01 22:16:06 -0400

    made questions loop through eachother -ng

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,33 +1,32 @@
 // list of all questions and choices
-// maybe we can use filler questions? 
+// maybe we can use filler questions?
 var questions = [
-    {
-        // would measure user's energy level
-      title: "How would active would you describe your lifestyle?",
-      choices: ["low", "moderate", "high"],
-    },
-    {
-        // would measure user's dog size
-      title: "What kind of home do you live in?",
-      choices: ["apartment", "house", "loft", "other"],
-    },
-    {
-        // would measure user's affection needs
-      title: "How dependent would you say to other people or things?",
-      choices: ["low", "moderate", "high"],
-    },
-    { 
-        // would measure user's dog dander
-      title: "How clean would you say you are?",
-      choices: ["dirty", "clean once a week", "OCD"],
-    },
-    {
-        // would measure user's dog dander
-      title: "Are you sensitive to dander?",
-      choices: ["Yes", "No"],
-    }
-  ];
-  
+  {
+    // would measure user's energy level
+    title: "How would active would you describe your lifestyle?",
+    choices: ["low", "moderate", "high"],
+  },
+  {
+    // would measure user's dog size
+    title: "What kind of home do you live in?",
+    choices: ["apartment", "house", "loft", "other"],
+  },
+  {
+    // would measure user's affection needs
+    title: "How dependent would you say to other people or things?",
+    choices: ["low", "moderate", "high"],
+  },
+  {
+    // would measure user's dog dander
+    title: "How clean would you say you are?",
+    choices: ["dirty", "clean once a week", "OCD"],
+  },
+  {
+    // would measure user's dog dander
+    title: "Are you sensitive to dander?",
+    choices: ["Yes", "No"],
+  },
+];
 
 // questions and answers:
 // zipcode: Can be manually entered by user
